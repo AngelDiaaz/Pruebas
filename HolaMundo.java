6d978ae330cd5530f6c8fa695ee0ca8b65e3e0e3
Angel Diaz Aviles <a_angel.diaz.aviles@iespablopicasso.es>
2021-11-09 12:41:01 +0100

    Primer commit

---
+++ HolaMundo.java
@@ -0,0 +1,21 @@
+package Tema1;
+
+import java.util.Scanner;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+	
+			Scanner sc = new Scanner(System.in);
+			int num = s.nextInt();
+			
+			if(num%2==0){
+				System.out.println(num + " es par");
+			}else{
+				System.out.println(num + " es impar");
+			}
+			
+			
+			sc.close();
+	}
+}
